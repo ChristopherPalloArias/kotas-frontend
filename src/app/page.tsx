980b9f598fd713c2e6b958dd0e559b65f1f99c68
ChristopherPalloArias <cipallo@uce.edu.ec>
2024-07-09 23:53:38 -0500

    fix: :bug: Change in time real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-bold">Minimarket Product Management</h1>
+      <h1 className="text-3xl font-bold">Minimarket Product Management Change in time Real</h1>
       <AddProduct onAddProduct={fetchProducts} />
       <button className="bg-blue-500 text-white p-2 mt-2" onClick={fetchProducts}>
         List Products
